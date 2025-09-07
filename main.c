0a1ca114ac42d1da51180b856c7d0402c6234ad4
Tim Speer <94493935+tim-speer@users.noreply.github.com>
2025-09-07 12:06:23 -0700

    Started work on display_menu function

--- main.c
+++ main.c
@@ -4,13 +4,52 @@
 #include "matrixprop.h"
 #include "matrixring.h"
 
-int main(void) {
-
-  MatrixRing matrix_ring = create_matrix_ring(2, 8);
+unsigned int get_matrix_ring_dim(void) {
+  unsigned int dim;
   
-  iter_calc_matrix_ring_props(&matrix_ring, 10);
+  printf("\nEnter Matrix Dimensions: ");
+  int n = scanf("%u", &dim);
+  if (n == 0 || dim < 2) {
+    printf("Failed to load matrix ring. Dimensions should be at least 2.\n");
+    dim = 0;
+  }
 
-  print_matrix_ring(matrix_ring);
-  free_matrix_ring(&matrix_ring);
+  return dim;
+}
+
+MatrixRing load_matrix_ring(void) {
+  MatrixRing matrix_ring;
+  printf("\n*******************");
+  printf("\nLoading Matrix Ring");
+  printf("\n*******************");
+  unsigned int dim = get_matrix_ring_dim(); 
+
+  return matrix_ring;
+}
+
+void display_menu(void) {
+  char in = '?';
+  MatrixRing matrix_ring;
+
+  while(in != 'q') {
+    printf("\n********************");
+    printf("\n(l) Load Matrix Ring");
+    printf("\n(q) Quit");
+    printf("\n********************");
+    printf("\nEnter Choice: ");
+    scanf("%s", &in);
+    switch(in) {
+      case 'l':
+        matrix_ring = load_matrix_ring();      
+      case 'q':
+        break;
+      default:
+        printf("!!!Invalid Choice!!!\n");
+    }
+  }
+}
+
+int main(void) {
+  display_menu();
   return EXIT_SUCCESS;
 }
