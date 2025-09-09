1ab2df5301831c85b686cce87d288fcfcfceb6f2
Tim Speer <94493935+tim-speer@users.noreply.github.com>
2025-09-09 07:50:08 -0700

    Added Print Matrix Ring option to menu

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ unsigned int get_matrix_ring_modulus(void) {
 
   printf("\nEnter Matrix Modulus: ");
   int n = scanf("%u", &modulus);
-  if (n == 0 || modulus < 2) {
+  if (n != 1 || modulus < 2) {
     printf("Failed to load matrix ring. Modulus should be at least 2.\n");
     modulus = 0;
   }
@@ -22,7 +22,7 @@ unsigned int get_matrix_ring_dim(void) {
   
   printf("\nEnter Matrix Dimensions: ");
   int n = scanf("%u", &dim);
-  if (n == 0 || dim < 2) {
+  if (n != 1 || dim < 2) {
     printf("Failed to load matrix ring. Dimensions should be at least 2.\n");
     dim = 0;
   }
@@ -31,30 +31,44 @@ unsigned int get_matrix_ring_dim(void) {
 }
 
 MatrixRing load_matrix_ring(void) {
-  MatrixRing matrix_ring;
   printf("\n*******************");
   printf("\nLoading Matrix Ring");
   printf("\n*******************");
-  unsigned int dim = get_matrix_ring_dim(); 
+
+  MatrixRing matrix_ring;
+  unsigned int dim = get_matrix_ring_dim();
+  if (dim == 0) {
+    matrix_ring = create_matrix_ring(2, 2);
+    return matrix_ring;
+  }
+ 
   unsigned int modulus = get_matrix_ring_modulus();
+  if (modulus == 0) {
+    matrix_ring = create_matrix_ring(2, 2);
+  } else {
+    matrix_ring = create_matrix_ring(dim, modulus);
+  }
 
   return matrix_ring;
 }
 
 void display_menu(void) {
   char in = '?';
-  MatrixRing matrix_ring;
+  MatrixRing matrix_ring = create_matrix_ring(2, 2);
 
   while(in != 'q') {
     printf("\n********************");
     printf("\n(l) Load Matrix Ring");
+    printf("\n(p) Print Matrix Ring");
     printf("\n(q) Quit");
     printf("\n********************");
     printf("\nEnter Choice: ");
     scanf("%s", &in);
     switch(in) {
       case 'l':
-        matrix_ring = load_matrix_ring();      
+        matrix_ring = load_matrix_ring();
+      case 'p':
+        print_matrix_ring(matrix_ring);      
       case 'q':
         break;
       default:
