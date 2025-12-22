3af81ef6e9aa8c74a79381d02231e98eb6921f5d
Tim Speer <94493935+tim-speer@users.noreply.github.com>
2025-12-22 11:01:30 -0700

    Added Eq IntMod instance

--- haskell/intmod.hs
+++ haskell/intmod.hs
@@ -1,5 +1,22 @@
-data IntMod = IntMod { value :: Int, modulus :: Int }
+data IntMod = IntMod { value :: Int, modulus :: Int } | Invalid
   deriving (Show)
 
+instance Num IntMod where
+  x + y | modulus x == modulus y = reduceIntMod $ IntMod (value x + value y) (modulus x)
+        | otherwise = Invalid
+  x * y | modulus x == modulus y = reduceIntMod $ IntMod (value x * value y) (modulus x)
+        | otherwise = Invalid 
+  abs x = reduceIntMod x
+  signum x = Invalid
+  fromInteger x = Invalid
+  negate Invalid = Invalid
+  negate (IntMod value modulus) = reduceIntMod $ IntMod (-value) modulus
+
+instance Eq IntMod where
+  x == y = (modulus x == modulus y) && (mod (value x - value y) (modulus x) == 0) 
+
 reduceIntMod :: IntMod -> IntMod
-reduceIntMod (IntMod value modulus) = IntMod (mod value modulus) modulus 
+reduceIntMod Invalid = Invalid
+reduceIntMod (IntMod value modulus) 
+  | modulus >= 2  = IntMod (mod value modulus) modulus
+  | otherwise = Invalid
