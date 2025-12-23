92355a1294a45ac501f91c16ebe4ad53b84988fc
Tim Speer <94493935+tim-speer@users.noreply.github.com>
2025-12-23 11:20:34 -0700

    Added createMatrixMod function

--- haskell/matrixmod.hs
+++ haskell/matrixmod.hs
@@ -1,5 +1,18 @@
 module MatrixMod where
 
+import IntMod
+
+data MatrixMod = MatrixMod { dim :: Int, modulus :: Int, entries :: [IntMod] }
+  deriving (Show)
+
+createMatrixMod :: Int -> Int -> [Int] -> MatrixMod
+createMatrixMod dim modulus entries
+  | dim >= 1 && 
+    modulus >= 2 &&
+    length entries == dim^2 = MatrixMod dim modulus (map convert entries) 
+  | otherwise = createMatrixMod 2 2 [0, 0, 0, 0]
+  where convert = (\v -> createIntMod v modulus)
+
 tuples :: Int -> Int -> [[Int]]
 tuples _ 0 = []
 tuples items 1 = [[x] | x <- [0..items-1]]
